fd321281c872ceb24ea20950e9e29f83b4a82417
GLR-901 <gabriel.l.romero@gmail.com>
2020-07-08 23:04:50 -0400

    Seeded Data for Testing

--- boardgames_app_api/db/seeds.rb
+++ boardgames_app_api/db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Has Been Seeded already!
+# Boardgame.create([
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254920151-51ulRXlJ7LL.jpg' , name: 'Gloomhaven', min_players: 1, max_players: 4, min_playtime: 60, max_playtime: 150, publisher: 'Cephalonfair Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254971843-513-lrmjs2L.jpg', name: 'Scythe', min_players: 1, max_players: 5, min_playtime: 90, max_playtime: 120, publisher: 'Stonemaier Games' },
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1540147295104', name: 'Root', min_players: 2, max_players: 4, min_playtime: 60, max_playtime: 90, publisher: 'Leder Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254894908-61llEAverzL.jpg', name: 'Viticulture: Essential Edition', min_players: 1, max_players: 6, min_playtime: 90, max_playtime: 120, publisher: 'Stonemaier Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1545152463996', name: 'Wingspan', min_players: 1, max_players: 5, min_playtime: 40, max_playtime: 70, publisher: 'Stonemaier Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254941010-61PJxjjnbfL.jpg', name: 'Spirit Island', min_players: 1, max_players: 4, min_playtime: 90, max_playtime: 120, publisher: 'Greater Than Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254200327-61EFZADvURL.jpg', name: 'Azul', min_players: 2, max_players: 4, min_playtime: 30, max_playtime: 60, publisher: 'Next Move Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254934477-61ys4u7ejfL.jpg', name: 'Terraforming Mars', min_players: 1, max_players: 5, min_playtime: 90, max_playtime: 120, publisher: 'Stronghold Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254183526-51f6ysXCMmL.jpg', name: 'Codenames', min_players: 2, max_players: 8, min_playtime: 15, max_playtime: 20, publisher: 'Czech Games Edition'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254186140-51iNoyxoamL.jpg', name: 'Pandemic', min_players: 2, max_players: 4, min_playtime: 45, max_playtime: 60, publisher: 'Z-Man Games, Inc.'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559253945470-61dlm9QcXpL.jpg', name: '7 Wonders', min_players: 2, max_players: 7, min_playtime: 30, max_playtime: 45, publisher: 'Repos Production'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254282618-61HduAZGE7L.jpg', name: 'Carcassonne', min_players: 2, max_players: 5, min_playtime: 30, max_playtime: 45, publisher: 'Z-Man Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559257833516-612BUfjuA7fL.jpg', name: 'Pandemic Legacy: Season 1', min_players: 2, max_players: 4, min_playtime: 60, max_playtime: 75, publisher: 'Z-Man Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559255812092-51S3zQnsPBL.jpg', name: '7 Wonders Duel', min_players: 2, max_players: 2, min_playtime: 30, max_playtime: 45, publisher: 'Asmodee Editions'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254202422-51mP2aJfyxL.jpg', name: 'The Castles of Burgundy', min_players: 2, max_players: 4, min_playtime: 30, max_playtime: 90, publisher: 'Ravensburger'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1538065470418', name: 'Concordia', min_players: 2, max_players: 5, min_playtime: 90, max_playtime: 120, publisher: 'Rio Grande Games'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254164722-51AHDwGznvL.jpg', name: 'Splendor', min_players: 2, max_players: 4, min_playtime: 30, max_playtime: 60, publisher: 'Asmodee Editions'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254202421-61wLscAHHSL.jpg', name: 'Ticket To Ride', min_players: 2, max_players: 5, min_playtime: 45, max_playtime: 90, publisher: 'Days of Wonder'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254194904-61JlvFn9fqL.jpg', name: 'Santorini', min_players: 2, max_players: 4, min_playtime: 15, max_playtime: 30, publisher: 'Spin Master'},
+#     { box_art: 'https://d2k4q26owzy373.cloudfront.net/700x700/games/uploaded/1559254997949-61iejxLClRL.jpg', name: 'Patchwork', min_players: 2, max_players: 2, min_playtime: 15, max_playtime: 30, publisher: 'Lookout Games'},
+# ])
+
+# puts "Seed Successful!"
